9da50830bc373eec9d81a39ca69836d1adeb9eb3
mohamedali3521 <123059063+mohamedali3521@users.noreply.github.com>
2023-12-14 10:28:48 -0800

    Add files via upload

---
+++ bubble_sort.java
@@ -0,0 +1,24 @@
+package DSA;
+
+public class bubble_sort {
+    public static void main(String[] args) {
+        int arr[]={21,32,11,44,64,12,12};
+        int n=arr.length;
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=0;j<n-i-1;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for(int v:arr)
+        {
+            System.out.print(v+" ");
+        }
+        }
+}
